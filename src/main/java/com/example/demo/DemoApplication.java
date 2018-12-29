930c6dc16024669020677cd203575a45c18d302a
Simon <nobdy@163.com>
2018-12-29 21:08:30 +0800

    add demo-spring

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    @GetMapping("/")
+    String home() {
+        return "<pre style='font-size: 16px; font-family: none;'>" +
+                "Demo: Spring Boot is here! built on 20181229.\n" +
+                "        # support env unset\n" +
+                "</pre>";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
